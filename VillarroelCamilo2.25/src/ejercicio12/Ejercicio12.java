eb97402977bd72f26f04f127915af8d7b632d15d
Camilo Villarroel <camilitovillarroel@gmail.com>
2025-10-29 19:44:28 +0100

    commit

--- VillarroelCamilo2.25/src/ejercicio12/Ejercicio12.java
+++ VillarroelCamilo2.25/src/ejercicio12/Ejercicio12.java
@@ -13,19 +13,19 @@ public class Ejercicio12 {
 		System.out.print("Introduzca a partir de que numero quiere saberse la secuencia de Fibonacci: ");
 		numero = teclado.nextInt();
 		previo = numero;
-		
+
 		System.out.print("Ahora introduzca hasta que dato quiera saber de la secuencia: ");
 		size = teclado.nextInt();
-		
-		System.out.print(numero+" ");
-		
+
+		System.out.print(numero + " ");
+
 		for (int i = 0; i < size; i++) {
-			
-			System.out.print(numero+" ");
-		
-			numero = numero+previo;
+
+			System.out.print(numero + " ");
+
+			numero = numero + previo;
 			previo = numero - previo;
-			
+
 		}
 
 	}
