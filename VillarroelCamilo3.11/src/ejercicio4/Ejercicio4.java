e700cc4b936a941f0586d57b4d8000975dc5efda
Camilo <camilitovillarroel@gmail.com>
2025-10-31 14:59:02 +0100

    Evoluciñon del ejercicio4 de matrices

--- VillarroelCamilo3.11/src/ejercicio4/Ejercicio4.java
+++ VillarroelCamilo3.11/src/ejercicio4/Ejercicio4.java
@@ -10,7 +10,7 @@ public class Ejercicio4 {
 
 		final int ALUMNOS = 4, ASIGNATURAS = 5;
 
-		int notaMinima, notaMaxima, notaMedia;
+		int notaMinima, notaMaxima, notaMedia = 0;
 
 		int[][] notas = new int[ALUMNOS][ASIGNATURAS];
 
@@ -22,10 +22,38 @@ public class Ejercicio4 {
 
 			for (int asignatura = 0; asignatura < 5; asignatura++) {
 
-				System.out.println("Nota " + (asignatura + 1) + "ª: ");
+				System.out.print("Nota " + (asignatura + 1) + "ª: ");
 				notas[alumno][asignatura] = teclado.nextInt();
 
 			}
+		}
+
+		for (int alumno = 0; alumno < ALUMNOS; alumno++) {
+
+			notaMinima = notas[alumno][0];
+
+			notaMaxima = notas[alumno][0];
+
+			for (int asignatura = 0; asignatura < 5; asignatura++) {
+
+				if (notas[alumno][asignatura] < notaMinima) {
+					notaMinima = notas[alumno][asignatura];
+				}
+				if (notaMaxima < notas[alumno][asignatura]) {
+					notaMaxima = notas[alumno][asignatura];
+				}
+
+				notaMedia = notaMedia + notas[alumno][asignatura];
+				System.out.println("\nnota actual agregada: "+notas[alumno][asignatura]);
+
+			}
+			
+			notaMedia = notaMedia/ASIGNATURAS;
+
+			System.out.println("La nota maxima del alumno nº " + (alumno + 1) + " es un " + notaMaxima);
+			System.out.println("La nota minima del alumno nº " + (alumno + 1) + " es un " + notaMinima);
+			System.out.println("La nota media del alumno nº " + (alumno + 1) + " es un " + notaMedia+"\n");
+
 		}
 
 	}
